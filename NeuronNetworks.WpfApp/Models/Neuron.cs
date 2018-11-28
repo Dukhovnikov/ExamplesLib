6f0818724c1728469e7c37d14ea52d38fe148e38
dduhovnikov <dduhovnikov@baltbet.ru>
2018-11-28 15:27:43 +0300

    Добавлены классы матрицы и вектора. Начто написание ОО нейронной сети Хэмминга

--- NeuronNetworks.WpfApp/Models/Neuron.cs
+++ NeuronNetworks.WpfApp/Models/Neuron.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CommonData.ClassLib.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,55 @@ namespace NeuronNetworks.WpfApp.Models
 {
     public class Neuron
     {
-
+        public double[] Weights;
+        public double Y;
+        public double S;
     }
+
+    public class HammingNetwork
+    {
+        /// <summary>Разрядность координат (размерность вектора)</summary>
+        public int N { get; set; }
+
+        /// <summary>Количество образцов</summary>
+        public int M { get; set; }
+
+        public Neuron[] FirstLayer;
+        public Neuron[] SecondLayer;
+
+        public Matrix WeightCoefficientMatrix;
+
+        /// <summary>Заполнение весовых коэффициентов нейронов первого слоя</summary>
+        public void InitializeNetwork(Vector[] sampleVectors)
+        {
+            N = sampleVectors[0].Size;
+            M = sampleVectors.Length;
+
+            FirstLayer = new Neuron[M];
+
+            WeightCoefficientMatrix = new Matrix(N, M);
+
+            for (int j = 0; j < M - 1; j++)
+            {
+                for (int i = 0; i < N - 1; i++)
+                {
+                    WeightCoefficientMatrix[i, j] = sampleVectors[j][i];
+                }
+            }
+        }
+
+        public void FillFirstLayer(Vector x)
+        {
+            double t = N / 2;
+
+            for (int j = 0; j < M; j++)
+            {
+                Neuron currentNeuron = FirstLayer[j];
+
+                double state = 0;
+            }
+        }
+    }
+
+
 }
